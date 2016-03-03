16378c932c481a3156b06f5d1a14ffa08b225cc2
Jim Dixon <jddixon@gmail.com>
2016-03-03 10:14:51 -0800

    switch to waf build utility

---
+++ include/fusegen_c.h
@@ -0,0 +1,3 @@
+/* fusegen_c/include/fusegen_c.h */
+
+#include <stdio.h>
---
+++ src/main.c
@@ -0,0 +1,21 @@
+/* fusegen_c/src/main.c */
+
+#include <stdio.h>
+#include <stdlib.h>         // for exit
+#include <getopt.h>         // for getopt_long
+
+#include "fusegen_c.h"
+
+// xlreg_client.c
+
+int main (int argc, char **argv) {
+
+    printf("hello and goodbye\n");
+    exit(0);
+
+}
+
+
+
+
+
