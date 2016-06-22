227fe7485f656d1761dad79c25e0cb97e9410077
Jim Dixon <jddixon@gmail.com>
2016-06-22 11:26:32 -0700

    update .dvcz, add ghpDoc/{css,img}/

--- src/version.c
+++ src/version.c
@@ -2,5 +2,5 @@
 
 #include "fusegen_c.h"
 
-const char *VERSION      = "0.2.0";
-const char *VERSION_DATE = "2016-06-15";
+const char *VERSION      = "0.2.1";
+const char *VERSION_DATE = "2016-06-22";
