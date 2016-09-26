5bb2d17f30433b7b7050b908a3877b61860a411c
Jim Dixon <jddixon@gmail.com>
2016-09-25 17:24:10 -0700

    standardize licensing

--- src/version.c
+++ src/version.c
@@ -2,5 +2,5 @@
 
 #include "fusegen_c.h"
 
-const char *VERSION      = "0.2.1";
-const char *VERSION_DATE = "2016-06-22";
+const char *VERSION      = "0.2.2";
+const char *VERSION_DATE = "2016-09-25";
